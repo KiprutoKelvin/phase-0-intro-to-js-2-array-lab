02e29b03fd116c4354e04ed23832f6887f3ef518
Kiru <kelvin.kipruto@student.moringaschool.com>
2023-03-19 20:51:38 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    return cats.push('Ralph');
+}
+ function destructivelyPrependCat(name) {
+    return cats.unshift("Bob");
+ }   
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+function appendCat(name) {
+    const newCats = [...cats];
+    newCats.push(name);
+    return newCats;
+}
+function prependCat(name) {
+    return ["Arnold", ...cats];
+  }
+removeLastCat = () =>{
+    let lastCat = [...cats];
+    lastCat.pop();
+    return lastCat;
+}
+removeFirstCat= () => {
+    const newCats = cats.slice(1);
+    return newCats;
+  }
